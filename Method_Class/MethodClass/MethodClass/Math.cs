48dd25cb37889ffa551c20ec64137d0a1fd637e4
julianne-murdock <98274277+julianne-murdock@users.noreply.github.com>
2022-07-30 14:22:21 -0400

    Method Class

---
+++ Method_Class/MethodClass/MethodClass/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    public class Math
+    {
+        public void Add(int input1, int input2)
+        {
+            int result = input1 + 4;
+        }
+    }
+}
---
+++ Method_Class/MethodClass/MethodClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var addition = new Math();
+            int input1 = 5;
+            int input2 = 4;
+            addition.Add(input1, input2);
+            Console.WriteLine(input2);
+            Console.ReadLine();
+        }
+    }
+}
