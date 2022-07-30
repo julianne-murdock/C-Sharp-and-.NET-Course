5774d1d31d0120d016055874e9c62916a645b48a
julianne-murdock <98274277+julianne-murdock@users.noreply.github.com>
2022-07-30 13:30:35 -0400

    Methods Submission

---
+++ Methods_Submission/Main_Method/Main_Method/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    public class Math
+    {
+        public int Add(int data, int value)
+        {
+            return data + value;
+        }
+
+        public int Add(decimal data, int value)
+        {
+            int dData = Decimal.ToInt32(data);
+            return dData + value;
+        }
+
+        public int Add(string data, int value)
+        {
+            int sData = Int32.Parse(data);
+            return sData + value;
+        }
+    }
+}
---
+++ Methods_Submission/Main_Method/Main_Method/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi there! Type a number you want to use for the math operations today.");
+            int Choice = Convert.ToInt32(Console.ReadLine());
+            var Value = Choice;
+
+            var operatorObject = new Math();
+            int result = operatorObject.Add(Value, 14);
+
+            Console.WriteLine("The result of " + Choice + " + 14 is " + result);
+
+            decimal Value2 = 5.5m;
+            int result2 = operatorObject.Add(Value2, Value);
+            Console.WriteLine("The result of " + Choice + " + 5.5 returned as an int is " + result2);
+
+            string Value3 = "24";
+            int result3 = operatorObject.Add(Value3, Value);
+
+            Console.WriteLine("The result of " + Choice + " + '24' returned as an int is " + result3);
+            Console.ReadLine();
+        }
+    }
+}
