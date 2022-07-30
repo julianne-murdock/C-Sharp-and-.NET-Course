fc76955857e235af6c21311446a8df895c7bab13
julianne-murdock <98274277+julianne-murdock@users.noreply.github.com>
2022-07-30 14:03:14 -0400

    Methods Sub

---
+++ Methods_Submission/Methods_Sub/Methods_Sub/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Sub
+{
+    public class Class1
+    {
+        public int Add(int data, params int[] value)
+        {
+
+            int i = 2;
+
+            foreach (int num in value)
+            {
+                i *= num;
+            }
+
+            return data + i;
+        }
+    }
+}
---
+++ Methods_Submission/Methods_Sub/Methods_Sub/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Sub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var operatorObject = new Class1();
+
+
+            Console.WriteLine("Please input a number");
+            int fstChoice = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Optional input a second number");
+            try
+            {
+                int sndChoice = Convert.ToInt32(Console.ReadLine());
+
+                var result = operatorObject.Add(fstChoice, sndChoice);
+
+                Console.WriteLine("The result of the calculations is: " + result);
+
+                Console.ReadLine();
+
+            }
+            catch
+            {
+
+
+                var result = operatorObject.Add(fstChoice);
+
+                Console.WriteLine("The result of the calculations is: " + result);
+
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
