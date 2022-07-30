69abb2a1a284611299beaa5f00caa7227083ab18
julianne-murdock <98274277+julianne-murdock@users.noreply.github.com>
2022-07-30 13:48:34 -0400

    Calling Methods

---
+++ Caling_Methods/Calling Methods/Calling Methods/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+    public class Math
+    {
+        public int Add(int data, int value)
+        {
+            return data + value;
+        }
+
+        public int Sub(int data, int value)
+        {
+            return data - value;
+        }
+
+        public int Div(int data, int value)
+        {
+            return data / value;
+        }
+    }
+}
---
+++ Caling_Methods/Calling Methods/Calling Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+    class Program
+    {
+        public int Data { get; set; }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to compute:");
+            var Value = Convert.ToInt32(Console.ReadLine());
+
+            var operatorObject = new Math();
+            var result = operatorObject.Add(Value, 14);
+            Console.WriteLine("First result is " + result);
+            result = operatorObject.Sub(result, 3);
+            Console.WriteLine("The second result is " + result);
+            result = operatorObject.Div(result, 2);
+
+            Console.WriteLine("The final result is " + result);
+            Console.ReadLine();
+        }
+    }
+}
