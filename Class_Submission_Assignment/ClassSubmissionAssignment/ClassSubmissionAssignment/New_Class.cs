bb9f419938fbc30ca8f5a2ecb8c479b89a4f5cd0
julianne-murdock <98274277+julianne-murdock@users.noreply.github.com>
2022-08-01 13:46:58 -0400

    Class Submission

---
+++ Class_Submission_Assignment/ClassSubmissionAssignment/ClassSubmissionAssignment/New_Class.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmissionAssignment
+{
+    public class New_Class
+    {
+        public void div(int a)
+        {
+            int b = a / 2;
+            Console.WriteLine("Good job, the answer is " + b);
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Class_Submission_Assignment/ClassSubmissionAssignment/ClassSubmissionAssignment/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmissionAssignment
+{
+    class Program
+    {
+        static class information
+        {
+
+
+            public static string info = " it must be a positive number";
+
+
+
+            public static void details()
+            {
+                Console.WriteLine("Welcome to the Divider Program");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            information.details();
+
+
+            Console.WriteLine("Please enter a number");
+            Console.WriteLine("Rules : {0} ", information.info);
+            int Value = Convert.ToInt32(Console.ReadLine());
+
+            New_Class num = new New_Class();
+
+
+
+            int diff;
+            bool greater = Compare(0, Value, out diff);
+
+            if (greater)
+            {
+                Console.WriteLine(diff + " is not a positive number");
+
+
+                Console.ReadLine();
+            }
+            else
+            {
+                num.div(Value);
+                Console.ReadLine();
+            }
+
+
+        }
+
+        static bool Compare(int a, int b, out int difference)
+        {
+            difference = a + b;
+            return a > b;
+
+        }
+        static bool Compare(int a, int b)
+        {
+
+            return a > b;
+
+        }
+    }
+
+}
