0a661e07d3dd2e2a57150c8743010c7ed10add37
julianne-murdock <98274277+julianne-murdock@users.noreply.github.com>
2022-08-01 15:38:34 -0400

    DaysOfTheWeek

---
+++ Parsing_Enums_Submission/DaysOfTheWeek/DaysOfTheWeek/Days.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfTheWeek
+{
+    public class Days
+    {
+
+        public Day Day { get; set; }
+
+
+
+
+    }
+    public enum Day //Create an enum for the days of the week.
+    {
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5,
+        Saturday = 6,
+        Sunday = 7,
+    }
+
+
+}
---
+++ Parsing_Enums_Submission/DaysOfTheWeek/DaysOfTheWeek/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfTheWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome To the Day Of The Week App!");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("Please enter what day it is today... (ex 'Monday')");//Prompt the user to enter the current day of the week.
+            string dayAnswer = (Console.ReadLine());
+
+
+             try //try/catch block and have it print "Please enter an actual day of the week.” to the console if an error occurs.
+            {
+                Day day = (Day)Enum.Parse(typeof(Day), dayAnswer); //Assign the value to a variable of that enum data type you just created.
+                if (Enum.IsDefined(typeof(Day), day))
+                {
+                    Console.WriteLine("Good job!");
+                    Console.ReadLine();
+                }
+
+            }
+
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+                Console.ReadLine();
+            }
+        }
+
+
+    }
+}
