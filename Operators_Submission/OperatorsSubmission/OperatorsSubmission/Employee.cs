983ef1190407b4eb8df5db09242d74a5008d3b9e
julianne-murdock <98274277+julianne-murdock@users.noreply.github.com>
2022-08-01 15:19:13 -0400

    operators submission

---
+++ Operators_Submission/OperatorsSubmission/OperatorsSubmission/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmission
+{
+    class Employee : Person //overload the “==” operator so it checks if two Employee objects are equal by comparing their Id property. Remember that comparison operators must be overloaded in pairs.
+    {
+        public int ID { get; set; }
+
+        public static bool operator ==(Employee employee, Employee employee2)
+        {
+            if (employee.ID == employee2.ID)
+                return true;
+            else
+                return employee.ID == employee2.ID;
+        }
+
+        public static bool operator !=(Employee employee, Employee employee2)
+        {
+            return employee.ID != employee2.ID;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var emp = obj as Employee;
+            if (emp == null)
+                return false;
+
+            return this.ID.Equals(emp.ID);
+        }
+    }
+}
---
+++ Operators_Submission/OperatorsSubmission/OperatorsSubmission/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            employee.FirstName = "John";
+            employee.LastName = "Doe";
+            employee.ID = 24923;
+
+            Employee employee2 = new Employee();
+            employee2.FirstName = "Richard";
+            employee2.LastName = "Roe";
+            employee2.ID = 25667;
+
+            Console.WriteLine(employee.ID == employee2.ID); //do the ids match?
+
+            Console.ReadLine();
+        }
+    }
+}
