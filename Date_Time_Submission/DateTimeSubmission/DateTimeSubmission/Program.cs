3ae6db586e4e640eff727c6614991a9bb87adce9
julianne-murdock <98274277+julianne-murdock@users.noreply.github.com>
2022-08-02 21:51:52 -0400

    Date and Time Submission

---
+++ Date_Time_Submission/DateTimeSubmission/DateTimeSubmission/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeSubmission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
