33d4ac826fe150d0473c4644e32748067bb9a18b
julianne-murdock <98274277+julianne-murdock@users.noreply.github.com>
2022-08-02 21:10:37 -0400

    Twenty One Submission Card Game

---
+++ Twenty_One_Submission/TwentyOneSubmision/TwentyOneSubmision/Dealer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOneSubmision
+{
+    public class Dealer
+    {
+        public string Name { get; set; }
+        public Deck Deck { get; set; }
+        public int Balance { get; set; }
+
+        public void Deal(List<Card> Hand)
+        {
+            Hand.Add(Deck.Cards.First());
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");
+            Console.WriteLine(card);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\16083\School\Basic_C-_Programs\TwentyOne\TwentyOne\log.txt", true))
+            {
+                file.WriteLine(DateTime.Now);
+                file.WriteLine(card);
+            }
+            Console.WriteLine(Deck.Cards.First().ToString() + "\n");
+            Deck.Cards.RemoveAt(0);
+        }
+    }
+}
