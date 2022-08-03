2ce490a33262b4195f3bd5b6f1d9acf6ccc445e7
julianne-murdock <98274277+julianne-murdock@users.noreply.github.com>
2022-08-02 21:59:16 -0400

    Date Time Submission

--- Date_Time_Submission/DateTimeSubmission/DateTimeSubmission/Program.cs
+++ Date_Time_Submission/DateTimeSubmission/DateTimeSubmission/Program.cs
@@ -10,6 +10,23 @@ namespace DateTimeSubmission
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome To The Time Travel Program\n");
+            System.Threading.Thread.Sleep(1000);
+            DateTime date = DateTime.Now;
+            Console.WriteLine("The Time Is: " + date); //Prints the current date and time to the console.
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("\nHow Many Hours Into The Future Do You Want To Go?");
+            int answer = Convert.ToInt32(Console.ReadLine()); //Asks the user for a number.
+
+            TimeSpan time = new TimeSpan(0, answer, 0, 0);
+            DateTime combined = date.Add(time);
+            Console.WriteLine("The Time Is Now: " + combined); //Prints to the console the exact time it will be in X hours, X being the number the user entered in step 2.
+
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("\nThankyou for using The Time Travel Program");
+
+
+            Console.ReadLine();
         }
     }
 }
