7916ecc3a3d96baeadc27064f1ba6f155b1987fc
julianne-murdock <98274277+julianne-murdock@users.noreply.github.com>
2022-08-24 13:10:15 -0400

    Console App

---
+++ ChallengeSubmission/ChallengeSubmission/ChallengeSubmission/Pages/Privacy.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChallengeSubmission.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            DateTime localDate = DateTime.Now;
+            ViewData["TimeStamp"] = localDate;
+
+
+        }
+    }
+}
